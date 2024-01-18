c80edb8899721d120c563adedb61aed670c39118
Piotr Branewski <pbranewski@student.agh.edu.pl>
2024-01-18 10:13:27 +0100

    usunięcie niepotrzebnych plików. Nie jest w pełni skończone

--- darwinworld/src/main/java/UI/Images.java
+++ darwinworld/src/main/java/UI/Images.java
@@ -8,22 +8,12 @@ import java.nio.file.Paths;
 
 public class Images {
     public Image grassImage;
-    public Image Image1;
-    public Image Image2;
-    public Image Image3;
-    public Image Image4;
-    public Image Image5;
     public Image tunnelImage;
 
     public Images() {
         try {
             this.tunnelImage = new Image (new FileInputStream(String.valueOf(Paths.get(System.getProperty("user.dir"),"darwinworld","src","main","resources","tunel.png"))));
             this.grassImage = new Image(new FileInputStream(String.valueOf(Paths.get(System.getProperty("user.dir"),"darwinworld","src","main","resources","trawa.png"))));
-            this.Image1 = new Image(new FileInputStream(String.valueOf(Paths.get(System.getProperty("user.dir"),"darwinworld","src","main","resources","pig.png"))));
-            this.Image2 = new Image(new FileInputStream(String.valueOf(Paths.get(System.getProperty("user.dir"),"darwinworld","src","main","resources","malpa.png"))));
-            this.Image3 = new Image(new FileInputStream(String.valueOf(Paths.get(System.getProperty("user.dir"),"darwinworld","src","main","resources","pies.png"))));
-            this.Image4 = new Image(new FileInputStream(String.valueOf(Paths.get(System.getProperty("user.dir"),"darwinworld","src","main","resources","tygrys.png"))));
-            this.Image5 = new Image(new FileInputStream(String.valueOf(Paths.get(System.getProperty("user.dir"),"darwinworld","src","main","resources","kot.png"))));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
